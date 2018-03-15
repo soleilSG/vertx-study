70af62d19a30f056affbd3930fc3b771d9b199ea
soleil <1938615010@qq.com>
2018-03-15 19:17:54 +0800

    vertx-maven-starter initialized project

--- src/test/java/com/stardrin/soleil/vertx/study/test/MainVerticleTest.java
+++ src/test/java/com/stardrin/soleil/vertx/study/test/MainVerticleTest.java
@@ -1,4 +1,4 @@
-package io.vertx.starter;
+package com.stardrin.soleil.vertx.study.test;
 
 import io.vertx.core.Vertx;
 import io.vertx.ext.unit.Async;
@@ -8,6 +8,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import com.stardrin.soleil.vertx.study.MainVerticle;
 
 @RunWith(VertxUnitRunner.class)
 public class MainVerticleTest {
