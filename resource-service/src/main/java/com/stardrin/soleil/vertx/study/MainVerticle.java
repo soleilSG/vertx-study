ee11c346fc9d0c332983b76ecc25a2c0485798da
soleil <1938615010@qq.com>
2018-03-19 11:30:00 +0800

    Rest endpoint ServiceDiscovery

--- resource-service/src/main/java/com/stardrin/soleil/vertx/study/MainVerticle.java
+++ resource-service/src/main/java/com/stardrin/soleil/vertx/study/MainVerticle.java
@@ -2,6 +2,10 @@ package com.stardrin.soleil.vertx.study;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.servicediscovery.Record;
+import io.vertx.servicediscovery.ServiceDiscovery;
+import io.vertx.servicediscovery.types.HttpEndpoint;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -16,5 +20,21 @@ public class MainVerticle extends AbstractVerticle {
 				message.reply(json.put("message", "hello " + message.body()));
 			}
 		});
+
+		Router router = Router.router(vertx);
+		router.get("/").handler(rc -> rc.response().end("hello"));
+
+		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+
+		ServiceDiscovery discovery = ServiceDiscovery.create(vertx);
+		Record record = HttpEndpoint.createRecord("hello-service", "localhost", 8080, "/");
+		discovery.publish(record, ar -> {
+			if (ar.succeeded()) {
+				System.out.println("hello-service has been published successfuly");
+			} else {
+				System.out.println(ar.cause());
+			}
+		});
+
 	}
 }
--- rest-service/src/main/java/com/stardrin/soleil/study/vertx/RxMainVerticle.java
+++ rest-service/src/main/java/com/stardrin/soleil/study/vertx/RxMainVerticle.java
@@ -4,9 +4,14 @@
  */
 package com.stardrin.soleil.study.vertx;
 
+import io.vertx.core.http.HttpClient;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.eventbus.EventBus;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.servicediscovery.ServiceDiscovery;
+import io.vertx.reactivex.servicediscovery.ServiceReference;
 
 /**
  * @author soleil
@@ -15,17 +20,39 @@ import io.vertx.reactivex.core.eventbus.EventBus;
  */
 public class RxMainVerticle extends AbstractVerticle {
 
+	private ServiceDiscovery discovery;
+
 	@Override
 	public void start() throws Exception {
-		vertx.createHttpServer().requestHandler(req -> {
-			EventBus eb = vertx.eventBus();
+		Router router = Router.router(vertx);
+		router.get("/eventBus").handler(this::eventBusHandler);
+		router.get("/rest").handler(this::restHandler);
+		vertx.createHttpServer().requestHandler(router::accept).listen(8090);
 
-			eb.<JsonObject>rxSend("hello", "Luke").subscribe(x -> req.response().end(x.body().encodePrettily()), e -> {
-				e.printStackTrace();
-				req.response().setStatusCode(500).end(e.getMessage());
-			});
+		discovery = ServiceDiscovery.create(vertx);
 
-		}).listen(8090);
+	}
 
+	private void eventBusHandler(RoutingContext rct) {
+		EventBus eb = vertx.eventBus();
+		eb.<JsonObject>rxSend("hello", "Luke").subscribe(x -> rct.response().end(x.body().encodePrettily()), e -> {
+			e.printStackTrace();
+			rct.response().setStatusCode(500).end(e.getMessage());
+		});
+	}
+
+	private void restHandler(RoutingContext rct) {
+		discovery.getRecord(new JsonObject().put("name", "hello-service"), ar -> {
+			if (ar.succeeded() && ar.result() != null) {
+				ServiceReference ref = discovery.getReference(ar.result());
+				HttpClient client = ref.getAs(HttpClient.class);
+				client.getNow("/", response -> {
+					response.bodyHandler(buf -> {
+						rct.response().end(buf.toString());
+						ref.release();
+					});
+				});
+			}
+		});
 	}
 }
